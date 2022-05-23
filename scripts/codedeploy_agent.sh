3cdadbd52f451582c1863a94ab550014541392ca
CalebJGulledge <calebjgulledge@gmail.com>
2022-05-23 12:05:07 -0400

    code deploy script

--- scripts/codedeploy_agent.sh
+++ scripts/codedeploy_agent.sh
@@ -1,16 +1,20 @@
 #run below commands
+echo "(((((     updating yum 1/7    )))))"
 sudo yum update -y
-echo "(((((     updating yum    )))))"
+echo "(((((     installing ruby 2/7 )))))"
 sudo yum install ruby -y
-echo "(((((     installing ruby )))))"
+echo "(((((     updating yum 3/7    )))))"
 sudo yum install wget -y
-echo "(((((     updating yum    )))))"
 cd /home/ec2-user
+echo "(((((     Getting  CodeDeploy 4/7   )))))"
 wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto -y
-#sudo yum install -y python-pip
-sudo pip install awscli -y
+echo "(((((     Installing Python Pip 5/7   )))))"
+dnf install -y python3-pip
+echo "(((((     Installing AWSCLI 6/7   )))))"
+pip3 install awscli --upgrade --user
+echo "(((((     Installing Java 11 7/7   )))))"
 sudo yum install java-11-openjdk-devel
 
 #verify installation

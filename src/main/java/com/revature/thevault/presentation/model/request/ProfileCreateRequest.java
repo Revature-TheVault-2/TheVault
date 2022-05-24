9f650c7da9e2f7b3746c62546a8dde1551f07b1b
Hiroyuki0487 <Hiroyuki0487@users.noreply.github.com>
2022-05-24 16:28:07 -0400

    ff

--- src/main/java/com/revature/thevault/presentation/model/request/ProfileCreateRequest.java
+++ src/main/java/com/revature/thevault/presentation/model/request/ProfileCreateRequest.java
@@ -20,26 +20,5 @@ public class ProfileCreateRequest {
     String email;
     String phoneNumber;
     String address;
-<<<<<<< HEAD
-    public int getUserId() {
-        return 0;
-    }
-    public Object getFirstName() {
-        return null;
-    }
-    public Object getLastName() {
-        return null;
-    }
-    public Object getEmail() {
-        return null;
-    }
-    public Object getPhoneNumber() {
-        return null;
-    }
-    public Object getAddress() {
-        return null;
-    }
-=======
     float notificationAmount; // only added because tests were yelling at us
->>>>>>> 38f11cde1cbe751113059a7ef0020f8835f59afb
 }

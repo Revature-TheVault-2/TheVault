dd70bf61da4d464253a61e31613813ad32e35df6
CalebJGulledge <calebjgulledge@gmail.com>
2022-05-24 15:26:58 -0400

    angular might work?

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -2,10 +2,9 @@
 cd /home/ec2-user/server
 sudo java -jar -Dserver.port=80 \
     *.jar > /dev/null 2> /dev/null < /dev/null &
-
 cd
 cd TheVault/TheVaultAngular
 npm install
 npm audit fix
 cd
-nohup http-server TheVault/TheVaultAngualr/dist/the-vault -p 9001 -a 0.0.0.0 &
+nohup http-server TheVault/TheVaultAngular/dist/the-vault -p 9001 -a 0.0.0.0 &

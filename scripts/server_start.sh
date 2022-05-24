c0eb2174dd71b60dcd260c5e0ce8f130dac0d0db
CalebJGulledge <calebjgulledge@gmail.com>
2022-05-24 16:30:08 -0400

    angular might work? part 36

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -7,5 +7,5 @@ cd TheVault/TheVaultAngular
 npm install
 npm audit fix
 cd
-nohup http-server TheVault/TheVaultAngular/dist/the-vault -p 9001 -a 0.0.0.0 &
-    /dev/null 2> /dev/null < /dev/null &
+nohup http-server -a 0.0.0.0 TheVault/TheVaultAngular/dist/the-vault -p 9001 \
+   http-server > /dev/null 2> /dev/null < /dev/null &

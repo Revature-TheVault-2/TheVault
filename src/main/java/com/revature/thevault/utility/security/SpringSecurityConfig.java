76d3c45e343e22d5aa19538ddbb4af87b432ef96
rowejake <rowejake600@gmail.com>
2022-05-25 17:07:12 -0500

    Added DELETE method to the CORS filter.

--- src/main/java/com/revature/thevault/utility/security/SpringSecurityConfig.java
+++ src/main/java/com/revature/thevault/utility/security/SpringSecurityConfig.java
@@ -69,7 +69,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
 		configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type"));
 		configuration.setAllowCredentials(true);
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();

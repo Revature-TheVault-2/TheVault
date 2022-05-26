85054b38ab8414f7fb2fcbbb73912104ca75d36d
CalebJGulledge <calebjgulledge@gmail.com>
2022-05-26 18:05:57 -0400

    caleb messed up

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -9,8 +9,8 @@ http-server TheVault/TheVaultAngular/dist/the-vault -p 9001 \
 
 cd
 cd TheVault/
-mvn clean package
+mvn clean install -DskipTests
 cd
 cd /home/ec2-user/TheVault/target
-sudo java -jar demo-0.0.1-SNAPSHOT.jar -Dserver.port=9000 \
+java -jar demo-0.0.1-SNAPSHOT.jar \
     *.jar > /dev/null 2> /dev/null < /dev/null &

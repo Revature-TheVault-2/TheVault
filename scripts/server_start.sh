eb5771327419d62f1ed321c0b63b9cd7ffdf5e15
CalebJGulledge <calebjgulledge@gmail.com>
2022-05-26 16:41:20 -0400

    different port

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -2,11 +2,12 @@
 cd TheVault/TheVaultAngular
 npm install
 npm audit fix
+ng build
 cd
 nohup http-server -a 0.0.0.0 TheVault/TheVaultAngular/dist/the-vault -p 9001 \
    http-server > /dev/null 2> /dev/null < /dev/null &
 
 cd
-cd /home/ec2-user/server/target
+cd /home/ec2-user/TheVault/target
 sudo java -jar demo-0.0.1-SNAPSHOT.jar -Dserver.port=9000 \
     *.jar > /dev/null 2> /dev/null < /dev/null &

f52f1e49cb546dbaa62c25714afc504518d81118
CalebJGulledge <calebjgulledge@gmail.com>
2022-05-26 17:09:50 -0400

    server start script change

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -4,9 +4,12 @@ npm install
 npm audit fix
 ng build
 cd
-nohup http-server -a 0.0.0.0 TheVault/TheVaultAngular/dist/the-vault -p 9001 \
+http-server TheVault/TheVaultAngular/dist/the-vault -p 9001 \
    http-server > /dev/null 2> /dev/null < /dev/null &
 
+cd
+cd TheVault/
+mvn clean package
 cd
 cd /home/ec2-user/TheVault/target
 sudo java -jar demo-0.0.1-SNAPSHOT.jar -Dserver.port=9000 \

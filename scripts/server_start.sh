a21d192992f0bd43fb1330932c3735324276df0f
CalebJGulledge <calebjgulledge@gmail.com>
2022-05-27 14:59:27 -0400

    slight script change

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -10,7 +10,17 @@ http-server TheVault/TheVaultAngular/dist/the-vault -p 9001 \
 cd
 cd TheVault/
 mvn clean install -DskipTests
+echo =========================================================================
+echo "maven installed"
+echo =========================================================================
 cd
 cd TheVault/target/
+echo =========================================================================
+echo "In target folder"
+echo =========================================================================
 java -jar demo-0.0.1-SNAPSHOT.jar \
     *.jar > /dev/null 2> /dev/null < /dev/null &
+    
+echo =========================================================================
+echo "JAR running"
+echo =========================================================================

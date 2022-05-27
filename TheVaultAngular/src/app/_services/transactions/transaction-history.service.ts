e9ab4c4d04b8b132fb8522dbc44ec7a626531f93
Fred-Doell <fjdoell@gmail.com>
2022-05-27 12:17:49 -0400

    Updated IP to bethe EC2 instead of localhost

--- TheVaultAngular/src/app/_services/transactions/transaction-history.service.ts
+++ TheVaultAngular/src/app/_services/transactions/transaction-history.service.ts
@@ -7,7 +7,7 @@ import { Transaction } from 'src/app/models/transaction/transaction.model';
   providedIn: 'root'
 })
 export class TransactionHistoryService {
-  transactionUrl = `http://localhost:9000/transaction/history`;
+  transactionUrl = `http://ec2-50-17-120-160.compute-1.amazonaws.com:9000/transaction/history`;
   getUrl?: any
 
   private transHistory = 'api/transactions'

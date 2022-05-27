0c997b1dc85ae6a494cd6d0e79a83435339efd6c
Dillon Meier <58122941+Maplesushi@users.noreply.github.com>
2022-05-27 16:32:12 -0400

    Update server_start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -13,12 +13,6 @@ http-server TheVault/TheVaultAngular/dist/the-vault -p 9001 \
    http-server > /dev/null 2> /dev/null < /dev/null &
 
 cd
-cd TheVault/
-mvn clean install -DskipTests
-echo =========================================================================
-echo "maven installed"
-echo =========================================================================
-cd
 cd TheVault/target/
 echo =========================================================================
 echo "In target folder"
